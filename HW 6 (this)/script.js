c73e184f036545458af0f509419d8e7f3525e114
Arsen <westwest1488@gmail.com>
2022-05-26 22:23:03 +0300

    HW6update

--- HW 6 (this)/script.js
+++ HW 6 (this)/script.js
@@ -1,112 +1,112 @@
 `use strict`;
 
-// THIS
+// // THIS
 
-// Это ladder (лестница) – объект, который позволяет подниматься вверх и спускаться:
+// // Это ladder (лестница) – объект, который позволяет подниматься вверх и спускаться:
 
-let ladder = {
-  step: 0,
-  up() {
-    this.step++;
-    return this;
-  },
-  down() {
-    this.step--;
-    return this;
-  },
-  showStep: function () {
-    // показывает текущую ступеньку
-    console.log(this.step);
-  },
-};
+// let ladder = {
+//   step: 0,
+//   up() {
+//     this.step++;
+//     return this;
+//   },
+//   down() {
+//     this.step--;
+//     return this;
+//   },
+//   showStep: function () {
+//     // показывает текущую ступеньку
+//     console.log(this.step);
+//   },
+// };
 
-// Теперь, если нам нужно сделать несколько последовательных вызовов, мы можем выполнить это так:
+// // Теперь, если нам нужно сделать несколько последовательных вызовов, мы можем выполнить это так:
 
-ladder.up();
-ladder.up();
-ladder.down();
-ladder.showStep(); // 1
+// ladder.up();
+// ladder.up();
+// ladder.down();
+// ladder.showStep(); // 1
 
-// Измените код методов up, down и showStep таким образом, чтобы их вызов можно было сделать по цепочке, например так:
+// // Измените код методов up, down и showStep таким образом, чтобы их вызов можно было сделать по цепочке, например так:
 
-ladder.up().up().down().showStep(); // 1
-// Такой подход широко используется в библиотеках JavaScript.
+// ladder.up().up().down().showStep(); // 1
+// // Такой подход широко используется в библиотеках JavaScript.
 
-// Hillel
+// // Hillel
 
-// 1) Создайте объект triangle со следующими свойствами: aSide, bSide, cSide.
+// // 1) Создайте объект triangle со следующими свойствами: aSide, bSide, cSide.
 
-// Создайте метод setValues(), который вызывает функцию prompt и заполняет свойства aSide, bSide, cSide значениями.
+// // Создайте метод setValues(), который вызывает функцию prompt и заполняет свойства aSide, bSide, cSide значениями.
 
-// Создайте в объекте triangle метод для расчета периметра треугольника.
-// Создайте в объекте triangle метод, который проверяет является ли треугольник
-// равносторонним:
+// // Создайте в объекте triangle метод для расчета периметра треугольника.
+// // Создайте в объекте triangle метод, который проверяет является ли треугольник
+// // равносторонним:
 
-// Выведите информацию о созданном треугольнике (включая результат выполнения
+// // Выведите информацию о созданном треугольнике (включая результат выполнения
 
-// всех методов) в консоль.
+// // всех методов) в консоль.
 
-/* Initial object */
-let triangle = {
-  aSide: 0,
-  bSide: 0,
-  cSide: 0,
-  setValues: function () {
-    this.aSide = +prompt(`side a`);
-    this.bSide = +prompt(`side b`);
-    this.cSide = +prompt(`side c`);
-  },
-  getPerimeter: function () {
-    let perimetr = this.aSide + this.bSide + this.cSide;
-    return perimetr;
-  },
-  isEqualSides: function () {
-    return this.aSide === this.bSide && this.aSide === this.cSide;
-  },
-};
-// В консоли ожидаю увидеть:
+// /* Initial object */
+// let triangle = {
+//   aSide: 0,
+//   bSide: 0,
+//   cSide: 0,
+//   setValues: function () {
+//     this.aSide = +prompt(`side a`);
+//     this.bSide = +prompt(`side b`);
+//     this.cSide = +prompt(`side c`);
+//   },
+//   getPerimeter: function () {
+//     let perimetr = this.aSide + this.bSide + this.cSide;
+//     return perimetr;
+//   },
+//   isEqualSides: function () {
+//     return this.aSide === this.bSide && this.aSide === this.cSide;
+//   },
+// };
+// // В консоли ожидаю увидеть:
 
-triangle.setValues(); // for examle 3 3 3
-console.log(triangle.getPerimeter()); // 9
-console.log(triangle.isEqualSides()); // true
+// triangle.setValues(); // for examle 3 3 3
+// console.log(triangle.getPerimeter()); // 9
+// console.log(triangle.isEqualSides()); // true
 
-// 2) Создайте объект calculator с методами:
+// // 2) Создайте объект calculator с методами:
 
-// read() запрашивает prompt() для двух значений и сохраняет их как свойства объекта x, y
-// sum() возвращает сумму этих двух значений
-// multi() возвращает произведение этих двух значений
-// diff() возвращает разницу
-// div() возвращает частное
-// В консоли ожидаю увидеть:
+// // read() запрашивает prompt() для двух значений и сохраняет их как свойства объекта x, y
+// // sum() возвращает сумму этих двух значений
+// // multi() возвращает произведение этих двух значений
+// // diff() возвращает разницу
+// // div() возвращает частное
+// // В консоли ожидаю увидеть:
 
-let calculator = {
-  read() {
-    this.a = 12;
-    this.b = 12;
-  },
-  sum() {
-    let sum = this.a + this.b;
-    return sum;
-  },
-  multi() {
-    let multi = this.a * this.b;
-    return multi;
-  },
-  diff() {
-    let diff = this.a - this.b;
-    return diff;
-  },
-  div() {
-    let div = this.a / this.b;
-    return div;
-  },
-};
+// let calculator = {
+//   read() {
+//     this.a = 12;
+//     this.b = 12;
+//   },
+//   sum() {
+//     let sum = this.a + this.b;
+//     return sum;
+//   },
+//   multi() {
+//     let multi = this.a * this.b;
+//     return multi;
+//   },
+//   diff() {
+//     let diff = this.a - this.b;
+//     return diff;
+//   },
+//   div() {
+//     let div = this.a / this.b;
+//     return div;
+//   },
+// };
 
-calculator.read(); // for example 5 2
-console.log(` Sum ${calculator.sum()}`); // 7
-console.log(` multi ${calculator.multi()}`);
-console.log(` diff ${calculator.diff()}`);
-console.log(` div ${calculator.div()}`);
+// calculator.read(); // for example 5 2
+// console.log(` Sum ${calculator.sum()}`); // 7
+// console.log(` multi ${calculator.multi()}`);
+// console.log(` diff ${calculator.diff()}`);
+// console.log(` div ${calculator.div()}`);
 
 // 3)* (опциональная) Дан объект country и функция format():
 
@@ -126,19 +126,17 @@ function format(start, end) {
 
 // Допишите код так, чтобы в консоли появились строки указанные в комментариях.
 
-format.call(/* Ваш код */); // Ukraine
-format.apply(/* Ваш код */); // [Ukraine]
-format.call(/* Ваш код */); // Kyiv
-format.apply(/* Ваш код */); // Kyiv
-format.apply(/* Ваш код */); // undefined
+format.call(country, "", ""); // Ukraine
+format.apply(country, ["[", "]"]); // [Ukraine]
+format.call(country.capital, "", ""); // Kyiv
+format.apply(country.capital, ["", ""]); // Kyiv
+format.apply(country.capitals, ["undef", "ined"]); // undefined   //  за то undefined))))
 
-// НЕ ПОЙМУ ВООБЩЕ ЧТО ЭТО
-
-// // 4) Что выведет данный код на экран и почему?
+// 4) Что выведет данный код на экран и почему?
 
 var text = "outside";
 function logIt() {
   console.log(text);
-  var text = "inside";
+  let text = "inside";
 }
 logIt();
